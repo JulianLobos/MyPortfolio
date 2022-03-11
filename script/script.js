0bf049a77eacd1cf8cacdb5d191b36072b26f38d
julianLobos <julianign@gmail.com>
2022-03-11 14:18:21 -0300

    football club page finished, full responsive

--- script/script.js
+++ script/script.js
@@ -1,3 +1,14 @@
+// smooth scrolling 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
 // hamburger button index
 
 $("#hamburger").click(function() {
@@ -16,4 +27,92 @@ $("#hamburgerSup").click(function() {
 $("#hamburgerFc").click(function() {
   $(this).toggleClass("on");
   $("#menuFc").slideToggle();
+});
+
+/* if (window.innerWidth < 576) {
+
+  $("#news").click(function() {
+    $("#hamburgerFc").toggleClass("on");
+    $("#menuFc").slideUp();
+  });
+
+  $("#results").click(function() {
+    $("#hamburgerFc").toggleClass("on");
+    $("#menuFc").slideUp();
+  });
+
+  $("#nextMatches").click(function() {
+    $("#hamburgerFc").toggleClass("on");
+    $("#menuFc").slideUp();
+  });
+
+  $("#gallery").click(function() {
+    $("#hamburgerFc").toggleClass("on");
+    $("#menuFc").slideUp();
+  });
+
+  $("#theClub").click(function() {
+    $("#hamburgerFc").toggleClass("on");
+    $("#menuFc").slideUp();
+  });
+
+  $("#homeBtn").click(function() {
+    if ($("#menuFc").is(":visible")){
+      $("#menuFc").slideUp();
+      $("#hamburgerFc").toggleClass("on");
+    }
+  });
+}
+*/
+
+const mq = window.matchMedia( "(max-width: 576px)" );
+
+if (mq.matches) {
+  $("#news").click(function() {
+    $("#hamburgerFc").toggleClass("on");
+    $("#menuFc").slideUp();
+  });
+
+  $("#results").click(function() {
+    $("#hamburgerFc").toggleClass("on");
+    $("#menuFc").slideUp();
+  });
+
+  $("#nextMatches").click(function() {
+    $("#hamburgerFc").toggleClass("on");
+    $("#menuFc").slideUp();
+  });
+
+  $("#gallery").click(function() {
+    $("#hamburgerFc").toggleClass("on");
+    $("#menuFc").slideUp();
+  });
+
+  $("#theClub").click(function() {
+    $("#hamburgerFc").toggleClass("on");
+    $("#menuFc").slideUp();
+  });
+
+  $("#homeBtn").click(function() {
+    if ($("#menuFc").is(":visible")){
+      $("#menuFc").slideUp();
+      $("#hamburgerFc").toggleClass("on");
+    }
+  });
+} else {
+  $("#menuFc").show();
+}
+
+$(document).ready(function(){
+  $("select").change(function(){
+      $(this).find("option:selected").each(function(){
+          var optionValue = $(this).attr("value");
+          if(optionValue){
+              $(".tabla").not("." + optionValue).hide();
+              $("." + optionValue).show();
+          } else{
+              $(".tabla").hide();
+          }
+      });
+  }).change();
 });
